0f160d0d11afc50f1f4b6bb4ab7237a87187eb1a
pedrodaa <44686019+pedrodaa@users.noreply.github.com>
2025-06-23 23:38:00 +0000

    Delete hello.ts

--- hello.ts
+++
@@ -1,7 +0,0 @@
-exports.handler = async function(event: any) {
-  console.log("Event:", JSON.stringify(event));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
-  };
-};
