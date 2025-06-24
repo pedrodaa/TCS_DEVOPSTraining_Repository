0d8c02036280c398b23105b460f16ade470d206b
pedrodaa <44686019+pedrodaa@users.noreply.github.com>
2025-06-24 15:51:58 +0000

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -2,6 +2,6 @@ exports.handler = async function(event: any) {
   console.log("Event:", JSON.stringify(event));
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello from Lambda!" }),
+    body: JSON.stringify({ message: "Hello from GitHub!" }),
   };
 };
